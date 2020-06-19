83396e735256bd9a14c1ffa9a43f522fef8a89e4
Necromos <necromos@necroworks.net>
2020-06-19 18:08:35 +0200

    fix: fixed issue with GraphQL handling
    Plugin states that it works for issues and pull requests, unfortunately
    that's not the case. `getCommiters()` code only relates to pull_requests
    GitHub GraphQL information where issues response structure is different.
    
    Bound to: OCM-220

--- src/graphql.ts
+++ src/graphql.ts
@@ -4,12 +4,8 @@ import { context } from '@actions/github'
 import { CommittersDetails } from './interfaces'
 
 
-
-export default async function getCommitters() {
-    try {
-        let committers: CommittersDetails[] = []
-        let filteredCommitters: CommittersDetails[] = []
-        let response = await octokit.graphql(`
+const queryPullRequests = async (): Promise<any> => {
+    const response = await octokit.graphql(`
         query($owner:String! $name:String! $number:Int! $cursor:String!){
             repository(owner: $owner, name: $name) {
             pullRequest(number: $number) {
@@ -47,24 +43,89 @@ export default async function getCommitters() {
             }
         }
     }`.replace(/ /g, ''), {
-            owner: context.repo.owner,
-            name: context.repo.repo,
-            number: context.issue.number,
-            cursor: ''
-        })
-        response.repository.pullRequest.commits.edges.forEach(edge => {
-            let committer = extractUserFromCommit(edge.node.commit)
-            let user = {
-                name: committer.login || committer.name,
-                id: committer.databaseId || '',
-                pullRequestNo: context.issue.number
+        owner: context.repo.owner,
+        name: context.repo.repo,
+        number: context.issue.number,
+        cursor: ''
+    });
+    return response;
+}
+
+const queryIssues = async (): Promise<any> => {
+    const response = await octokit.graphql(`
+        query($owner:String! $name:String! $number:Int! $cursor:String!){
+            repository(owner: $owner, name: $name) {
+            issue(number: $number) {
+                participants(first: 100, after: $cursor) {
+                    totalCount
+                    edges {
+                        node {
+                            email
+                            name
+                            id
+                            databaseId
+                            login
+                        }
+                        cursor
+                    }
+                    pageInfo {
+                        endCursor
+                        hasNextPage
+                    }
+                }
             }
-            if (committers.length === 0 || committers.map((c) => {
-                return c.name
-            }).indexOf(user.name) < 0) {
-                committers.push(user)
-            }
-        })
+        }
+    }`.replace(/ /g, ''), {
+        owner: context.repo.owner,
+        name: context.repo.repo,
+        number: context.issue.number,
+        cursor: ''
+    });
+    return response;
+}
+
+
+export default async function getCommitters() {
+    try {
+        let committers: CommittersDetails[] = []
+        let filteredCommitters: CommittersDetails[] = []
+        let response: any
+        switch (context.eventName) {
+            case 'pull_request':
+                response = await queryPullRequests()
+                response.repository.pullRequest.commits.edges.forEach(edge => {
+                    let committer = extractUserFromCommit(edge.node.commit)
+                    let user = {
+                        name: committer.login || committer.name,
+                        id: committer.databaseId || '',
+                        pullRequestNo: context.issue.number
+                    }
+                    if (committers.length === 0 || committers.map((c) => {
+                        return c.name
+                    }).indexOf(user.name) < 0) {
+                        committers.push(user)
+                    }
+                })
+                break
+            case 'issue_comment':
+                response = await queryIssues()
+                response.repository.issue.participants.edges.forEach(edge => {
+                    const committer = edge.node
+                    let user = {
+                        name: committer.login || committer.name,
+                        id: committer.databaseId || '',
+                        pullRequestNo: context.issue.number
+                    }
+                    if (committers.length === 0 || committers.map((c) => {
+                        return c.name
+                    }).indexOf(user.name) < 0) {
+                        committers.push(user)
+                    }
+                })
+                break
+            default:
+                throw new Error(`Event ${context.eventName} not supported`)
+        }
         filteredCommitters = committers.filter((committer) => {
             return committer.id !== 41898282
         })
