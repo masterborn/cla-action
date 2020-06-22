f5a7035dac79828b2e5f781ecb6ca0511e4c69df
Necromos <necromos@necroworks.net>
2020-06-22 19:20:31 +0200

    Adding test for persistance

--- src/octokit.ts
+++ src/octokit.ts
@@ -5,7 +5,7 @@ import * as core from '@actions/core'
 const octokit = new GitHub(process.env.GITHUB_TOKEN as string)
 
 const persistanceRepository = core.getInput('persistance-repository')
-const persistanceOctokit = persistanceRepository !== '' ? new GitHub(process.env.PERSISTANCE_GITHUB_TOKEN as string) : octokit
+const persistanceOctokit = persistanceRepository !== '' && process.env.PERSISTANCE_GITHUB_TOKEN ? new GitHub(process.env.PERSISTANCE_GITHUB_TOKEN as string) : octokit
 
 export default octokit 
 export { octokit, persistanceOctokit }
