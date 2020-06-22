9eb4a9199e9b336d0c238f199ab8d3e54be2e5d9
Necromos <necromos@necroworks.net>
2020-06-22 19:33:25 +0200

    Testing out no-token request

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,11 +1,12 @@
 import { GitHub } from '@actions/github'
+import Octokit from '@octokit/rest'
 import * as core from '@actions/core'
 
 
 const octokit = new GitHub(process.env.GITHUB_TOKEN as string)
 
 const persistanceRepository = core.getInput('persistance-repository')
-const persistanceOctokit = persistanceRepository !== '' && process.env.PERSISTANCE_GITHUB_TOKEN ? new GitHub(process.env.PERSISTANCE_GITHUB_TOKEN as string) : octokit
+const persistanceOctokit = persistanceRepository !== '' && process.env.PERSISTANCE_GITHUB_TOKEN ? new GitHub(process.env.PERSISTANCE_GITHUB_TOKEN as string) : new Octokit()
 
 export default octokit 
 export { octokit, persistanceOctokit }
