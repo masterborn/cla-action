68ff546983e2ad1014cd52ec8cbdb4aef1e316a7
Necromos <necromos@necroworks.net>
2020-06-30 14:47:46 +0200

    Adding octokit

--- src/inits/octokit.ts
+++ src/inits/octokit.ts
@@ -1,10 +1,11 @@
 import { getOctokit } from '@actions/github'
+import { Octokit } from '@octokit/rest'
 import * as core from '@actions/core'
 import isForkedPRRun from '../common/isForkedPRRun'
 
 const octokit = getOctokit(process.env.GITHUB_TOKEN as string)
 
-const readOctokit = getOctokit('')
+const readOctokit = new Octokit();
 
 const persistanceRepository = core.getInput('persistance-repository')
 const persistanceOctokit = isForkedPRRun() ? readOctokit : getOctokit(process.env.PERSISTANCE_GITHUB_TOKEN as string)
