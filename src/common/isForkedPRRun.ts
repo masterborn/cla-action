35d38b7458b16cefb195bdd93c52a25bdec298b3
Necromos <necromos@necroworks.net>
2020-07-02 11:33:08 +0200

    Fixing isForkedPRRun comparison

--- src/common/isForkedPRRun.ts
+++ src/common/isForkedPRRun.ts
@@ -3,5 +3,5 @@ import * as core from '@actions/core'
 const persistanceRepository = core.getInput('persistance-repository')
 
 export default function isForkedPRRun() {
-  return persistanceRepository !== '' && !!process.env.PERSISTANCE_GITHUB_TOKEN;
+  return persistanceRepository !== '' && !process.env.PERSISTANCE_GITHUB_TOKEN;
 }
